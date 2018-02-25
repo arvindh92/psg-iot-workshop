5ac0c155e30072d4af864b3c20fa9e1c99ad78eb
Arvind <Arvind@192.168.0.5>
2018-02-25 15:15:23 +0530

    first push

---
+++ device/register.py
@@ -0,0 +1,27 @@
+'''
+Created on 25-Feb-2018
+
+@author: Arvind
+'''
+
+import sys
+import iothub_service_client
+from iothub_service_client import IoTHubRegistryManager, IoTHubRegistryManagerAuthMethod
+from iothub_service_client import IoTHubDeviceStatus, IoTHubError
+
+CONNECTION_STRING = "HostName=psg.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=xadV+l5hi1IwxmO5fxHFWThAHTrpaBGe0fI5UnTvz9c="
+DeviceID = "akdj"
+
+
+
+def print_device_info(title, iothub_device):
+    print ( title + ":" )
+    print ( "iothubDevice.deviceId                    = {0}".format(iothub_device.deviceId) )
+    print ( "iothubDevice.primaryKey                  = {0}".format(iothub_device.primaryKey) )
+    print ( "" )
+    
+iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+device_info = (iothub_registry_manager.get_device(DeviceID))
+
+print_device_info("DEVICE:"+DeviceID, device_info)
---
+++ device/subscribe.py
@@ -0,0 +1,79 @@
+'''
+Created on 25-Feb-2018
+
+@author: Arvind
+'''
+from paho.mqtt import client as mqtt
+
+import time
+from urllib.parse import quote_plus, urlencode, urlparse
+from base64 import b64encode, b64decode
+from hmac import HMAC
+from _sha256 import sha256
+from pip._vendor.distlib.compat import raw_input
+import threading
+
+
+
+device_id = "akdj"
+SASKey = "LhRz0Ce2gQ7gbEAcrYo19Rwh31Fju24LoH6wPIb1nT4="
+iot_hub_name = "psg"
+
+import ssl
+
+path_to_root_cert = "./rootca.pem"
+def get_sas():
+
+    uri ="{}/devices/{}".format(iot_hub_name+".azure-devices.net",device_id)
+    key = SASKey
+    policy_name = None
+    expiry = 30
+    ttl = time.time() + expiry
+    sign_key = "%s\n%d" % ((quote_plus(uri)), int(ttl))
+    signature = b64encode(HMAC(b64decode(key), sign_key.encode(encoding='utf_8'), sha256).digest())
+    rawtoken = {
+        'sr' :  uri,
+        'sig': signature,
+        'se' : str(int(ttl))
+        }
+    if policy_name is not None:
+        rawtoken['skn'] = policy_name
+    return 'SharedAccessSignature ' + urlencode(rawtoken)
+
+print(get_sas())
+sas_token = get_sas()
+pub_topic = "devices/" + device_id + "/messages/events/"
+sub_topic = "devices/"+device_id+"/messages/devicebound/#"
+
+
+def on_connect(client, userdata, flags, rc):
+    print ("Device connected with result code: " + str(rc))
+    client.subscribe(sub_topic) 
+def on_message(client, userdata, msg):
+    print(str(msg.payload))
+
+def on_disconnect(client, userdata, rc):
+    print ("Device disconnected with result code: ", userdata, str(rc))
+def on_publish(client, userdata, mid):
+    print ("Device sent message")
+
+client = mqtt.Client(client_id=device_id, protocol=mqtt.MQTTv311)
+
+client.on_connect = on_connect
+
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+client.on_publish = on_publish
+client.tls_set(ca_certs=path_to_root_cert, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
+client.tls_insecure_set(False)
+
+client.username_pw_set(username=iot_hub_name+".azure-devices.net/" + device_id+"/api-version=2016-11-14", password=sas_token)
+
+
+client.connect(iot_hub_name+".azure-devices.net", port=8883)
+       
+
+
+client.loop_forever()
+
+
